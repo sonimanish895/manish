c002caf2dc5ddfaa91d2c4a751039c5443b1463a
Manish Soni <sonimanish895@gmail.com>
2022-02-15 22:50:44 +0530

    THIRD COMMIT

--- parth.java
+++ parth.java
@@ -4,6 +4,7 @@ public class parth
 {
 public static void main(String args[])
 {
-System.out.println("HELLO WORLD");
+System.out.println("HELLO PARTH BHIA");
+System.out.println("DIL KHUS KITA HI");
 }
 }
